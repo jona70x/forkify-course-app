e0313edd8931142b6f8c1b35bb275e64cb787d1b
jona70x <jonathancarpio@Jonathans-MacBook-Pro.local>
2022-03-14 15:48:41 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// Constant variables
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIME_OUT = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1138dd87-b9e9-45cb-b543-7d9272b349a7';
+export const MODAL_CLOSE_SEC = 2.5;
